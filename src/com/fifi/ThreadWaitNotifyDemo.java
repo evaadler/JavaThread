d97f84812d4b4d7c794ff4087f44dc5f0e8d96a9
alicia <jiandanfifi>
2020-07-05 11:12:40 +0800

    基本的生产者消费者1.0

--- src/com/fifi/ThreadWaitNotifyDemo.java
+++ src/com/fifi/ThreadWaitNotifyDemo.java
@@ -7,9 +7,11 @@ import java.awt.image.VolatileImage;
  * 题目：现在两个线程，可以操作初始值为零的一个变量，
  * 实现一个线程对该变量加1，一个线程对该变量减1
  * 实现交替，来10轮，变量初始值为零
+ * 需求2 假设两个生产，两个消费
  *
  * 1） 高内聚低耦合前提下，线程操作资源类
  * 2） 判断/干活/通知
+ * 3) 多线程交互中，必须要防止多线程的虚假唤醒，也即（判断只用while，不能用if）
  *
  * @author Alicia
  * @description
@@ -22,30 +24,56 @@ public class ThreadWaitNotifyDemo {
     public static void main(String[] args) throws Exception{
         Data data = new Data();
 
-        for (int i=0; i<10; i++) {
+
             new Thread(() -> {
+                for (int i=0; i<10; i++) {
                 try {
                     data.add();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
-                }
+                }}
                 // System.out.println(Thread.currentThread().getName()+": " +data.number);
             }, "AAA").start();
 
             new Thread(() -> {
+                for (int i=0; i<10; i++) {
+                    try {
+                        data.sub();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                // System.out.println(Thread.currentThread().getName()+": " + data.number);
+            }, "BBB").start();
+
+
+        new Thread(() -> {
+            for (int i=0; i<10; i++) {
+                try {
+                    data.add();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }}
+            // System.out.println(Thread.currentThread().getName()+": " +data.number);
+        }, "CCC").start();
+
+
+
+        new Thread(() -> {
+            for (int i=0; i<10; i++) {
                 try {
                     data.sub();
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                // System.out.println(Thread.currentThread().getName()+": " + data.number);
-            }, "BBB").start();
+            }
+            // System.out.println(Thread.currentThread().getName()+": " + data.number);
+        }, "DDD").start();
+
+
+        //System.out.println("End:"+data.getNumber());
+
         }
-
-        System.out.println("End:"+data.getNumber());
-
-    }
-
 
 
 }
@@ -54,7 +82,7 @@ class Data{
     private int number = 0;
     public synchronized void add() throws InterruptedException {
         // 1. 判断
-        if (number != 0) {
+        while (number != 0) {
              this.wait();
         }
 
@@ -66,7 +94,7 @@ class Data{
 
     }
     public synchronized void sub() throws InterruptedException {
-        if (number == 0) {
+        while (number == 0) {
             this.wait();
         }
         number --;
