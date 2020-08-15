2015bceeb6470b2c4268d84acba61a2f4651e046
alicia <jiandanfifi>
2020-08-15 16:29:44 +0800

    CAS -- 原子引用

--- src/main/java/com/fifi/AtomicReferenceDemo.java
+++ src/main/java/com/fifi/AtomicReferenceDemo.java
@@ -1,5 +1,10 @@
 package com.fifi;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+import java.util.concurrent.atomic.AtomicReference;
+
 /**
  * AtomicReferenceDemo
  *
@@ -12,10 +17,17 @@ public class AtomicReferenceDemo {
     public static void main(String[] args) {
         User user1 = new User("lily", 22);
         User user2 = new User("makio", 15);
+
+        // 相当于把user1变成了原子
+        AtomicReference<User> ar = new AtomicReference<>();
+        ar.set(user1);
+
+        System.out.println(ar.compareAndSet(user1, user2) + "\t"+ar.get().toString());
+        System.out.println(ar.compareAndSet(user1, user2) + "\t"+ar.get().toString());
     }
 }
 
-
+@ToString
 @AllArgsConstructor
 class User{
 
